9fb64815a24449a7b2330d801cc67ad37ac293f2
Muhammad Fiqri <viqronization@gmail.com>
2017-07-31 06:19:59 +0700

    setup picasso to use less memory and set it memory cache size to 100 MB

--- app/src/main/java/net/crescenthikari/popmovies/PopMoviesApp.java
+++ app/src/main/java/net/crescenthikari/popmovies/PopMoviesApp.java
@@ -1,6 +1,10 @@
 package net.crescenthikari.popmovies;
 
 import android.app.Application;
+import android.graphics.Bitmap;
+
+import com.squareup.picasso.LruCache;
+import com.squareup.picasso.Picasso;
 
 import net.crescenthikari.popmovies.data.DataManager;
 import net.crescenthikari.popmovies.data.api.TmdbApiService;
@@ -20,6 +24,23 @@ public class PopMoviesApp extends Application
     @Override
     public void onCreate() {
         super.onCreate();
+        setupPicassoGlobalConfig();
+    }
+
+    private void setupPicassoGlobalConfig() {
+        Picasso picasso = new Picasso.Builder(this)
+                .defaultBitmapConfig(Bitmap.Config.RGB_565)
+                .memoryCache(new LruCache(100000000))
+                .build();
+        // set the global instance to use this Picasso object
+        // all following Picasso (with Picasso.with(Context context) requests will use this Picasso object
+        // you can only use the setSingletonInstance() method once!
+        try {
+            Picasso.setSingletonInstance(picasso);
+        } catch (IllegalStateException ignored) {
+            // Picasso instance was already set
+            // cannot set it after Picasso.with(Context) was already in use
+        }
     }
 
     @Override
